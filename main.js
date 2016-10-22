3033639c1813e2fb5ca27d99a7f5fcbcf4c78f1e
Lauren Watson <lawatson0719@gmail.com>
2016-10-22 09:36:42 -0400

    completed project

--- main.js
+++ main.js
@@ -3,6 +3,10 @@
     var backButton = document.querySelector('#back');
     var nextButton = document.querySelector('#next');
     var steps = document.querySelectorAll('.step');
+    var textArea0 = document.querySelector('#description');
+    var textArea2 = document.querySelector('#ingredients');
+    var textArea3 = document.querySelector('#steps');
+    var hours = document.querySelector('#hours');
     // currentStep should keep track of the index of current step that is visible.
     var currentStep = 0;
 
@@ -12,13 +16,22 @@
     // otherwise hide it. We call this function at the bottom of the scope to do
     // an initial show/hide.
     function updateButtons() {
-
-    }
+        if (currentStep <= 0) {
+            backButton.classList.remove('is-active');
+        } else {
+            backButton.classList.add('is-active');
+        };
+        if (currentStep < steps.length - 1) {
+            nextButton.classList.add('is-active');
+        } else {
+            nextButton.classList.remove('is-active');
+        };
+    };
 
     // `hideCurrentStep` should hide the step that is currently visible using
     // the `currentStep` variable.
     function hideCurrentStep() {
-
+        steps[currentStep].classList.remove('is-active');
     }
 
     // `next` should validate the inputs on the current step by calling
@@ -29,8 +42,14 @@
     // * `next` should also call `updateButtons` to display the appropriate
     // buttons.
     function next() {
-
+        if(validate() === true) {
+            hideCurrentStep();
+            currentStep++;
+            steps[currentStep].classList.add('is-active');
+            updateButtons();
+        } 
     }
+
 
     // `back` should validate the inputs on the current step by calling
     // `validate()`.
@@ -40,9 +59,15 @@
     // * `back` should also call `updateButtons` to display the appropriate
     // buttons.
     function back() {
-
+       if(validate() === true) {
+            hideCurrentStep();
+            currentStep--;
+            steps[currentStep].classList.add('is-active');
+            updateButtons();
+        }
     }
 
+    
     // Validate should evaluate the input of the inputs in the current step.
     // * The function should start by clearing any errors that were
     // previously there.
@@ -52,22 +77,62 @@
     // `data-error` attribute on the input's containing `.field` element to
     // an error message, add the class `is-error` to the field and
     // ultimately return false.
+
+    function showValidationError(el, msg) {
+        el.dataset.error = msg;
+        el.classList.add('is-error');
+    }
+
     function validate() {
         var valid = true;
 
         if (currentStep === 0) {
-            if (nameEl.value.length === 0) {
-                nameEl.parentElement.dataset.error = 'Required';
-                nameEl.parentElement.classList.add('is-error');
+            if (nameEl.value.length < 3 || nameEl.value.length > 50) {
+                showValidationError(nameEl.parentElement, 'Required');
+                // nameEl.parentElement.dataset.error = 'Required';
+                // nameEl.parentElement.classList.add('is-error');
+                valid = false;
+            };
+            if (textArea0.value.length < 20 || textArea0.value.length > 255) {
+                showValidationError(textArea0.parentElement, 'Required');
+                // textArea0.parentElement.dataset.error = 'Required';
+                // textArea0.parentElement.classList.add('is-error');
+                valid = false;
+            };
+
+        } else if (currentStep === 1) {
+            if (hours.value.length === 0) {
+                showValidationError(hours.parentElement, 'Required');
+                // hours.parentElement.dataset.error = 'Required';
+                // hours.parentElement.classList.add('is-error');
                 valid = false;
             }
-            /* etc. */
-        } else if (currentStep === 1) {
 
-        } /* etc. */
+        } else if (currentStep === 2) {
+            if (textArea2.value.length <= 0 || textArea2.value.length > 255) {
+                showValidationError(textArea2.parentElement, 'Required');
+                valid = false;
+            }
+
+        } else if (currentStep === 3) {
+            if (textArea3.value.length <= 0 || textArea3.value.length > 255) {
+                showValidationError(textArea3.parentElement, 'Required');
+                valid = false;
+            }
+        }
 
         return valid;
     }
+
+    nextButton.addEventListener('click', function(e) {
+        e.preventDefault();
+        next();
+    })
+
+    backButton.addEventListener('click', function(e) {
+        e.preventDefault();
+        back();
+    })
 
     // Register an event handler for the 'click' event on the next button.
     // The event handler should prevent any default functionality the
